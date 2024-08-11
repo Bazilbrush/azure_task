b57c72555b66d769667ac5f5786ecabaf642edf2
Bazilbrush <bazileuski.jack@gmail.com>
2024-08-11 13:09:33 +0100

    fixed incorrect referces

--- infra/modules/vm/backup.tf
+++ infra/modules/vm/backup.tf
@@ -6,15 +6,15 @@ resource "random_string" "random" {
 
 resource "azurerm_recovery_services_vault" "vm" {
   name                = "${random_string.random.result}-r-v-${terraform.workspace}"
-  location            = azurerm_resource_group.azure_task_primary.location
-  resource_group_name = azurerm_resource_group.azure_task_primary.name
+  location            = azurerm_resource_group.azure_task.location
+  resource_group_name = azurerm_resource_group.azure_task.name
   sku                 = "Standard"
 }
 
 
 resource "azurerm_backup_policy_vm" "vm" {
   name                = "${random_string.random.result}-r-v-p-${terraform.workspace}"
-  resource_group_name = azurerm_resource_group.azure_task_primary.name
+  resource_group_name = azurerm_resource_group.azure_task.name
   recovery_vault_name = azurerm_recovery_services_vault.vm.name
 
   backup {
@@ -27,7 +27,7 @@ resource "azurerm_backup_policy_vm" "vm" {
 }
 
 resource "azurerm_backup_protected_vm" "vm1" {
-  resource_group_name = azurerm_resource_group.azure_task_primary.name
+  resource_group_name = azurerm_resource_group.azure_task.name
   recovery_vault_name = azurerm_recovery_services_vault.vm.name
   source_vm_id        = azurerm_linux_virtual_machine.primary.id
   backup_policy_id    = azurerm_backup_policy_vm.vm.id
